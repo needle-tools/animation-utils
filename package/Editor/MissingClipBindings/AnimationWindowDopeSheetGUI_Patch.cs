70e1f6a8379190c2ec42d332fd48438ed2271f11
Marcel Wiessler <marcel@gaisterhand.de>
2022-09-03 09:59:40 +0200

    add patch to animationcurve compare method with cached strings to improve performance when moving many keyframes

---
+++ package/Editor/MissingClipBindings/AnimationWindowDopeSheetGUI_Patch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using HarmonyLib;
+using UnityEditor;
+using UnityEngine;
+
+namespace Needle.AnimationUtils
+{
+	public class AnimationWindowDopeSheetGUI_Patch
+	{
+		[InitializeOnLoadMethod]
+		private static void Init()
+		{
+			var instance = new Harmony("com.needle.animation-utils-dopesheet");
+			
+			// var dopeLines = AccessTools.Method(AnimationWindowDopeSheetAccess.GetDopeSheetType(), "DopelinesGUI");
+			// if (dopeLines != null)
+			// 	instance.Patch(dopeLines, new HarmonyMethod(AccessTools.Method(typeof(AnimationWindowDopeSheetGUI_Patch), nameof(DopeLinesGUI_Prefix))));
+			//
+			// var startLiveEdit = AccessTools.Method(AnimationWindowDopeSheetAccess.GetAnimEditorType(), "OnStartLiveEdit");
+			// if (startLiveEdit != null)
+			// 	instance.Patch(startLiveEdit, new HarmonyMethod(AccessTools.Method(typeof(AnimationWindowDopeSheetGUI_Patch), nameof(OnStartLiveEdit_Prefix))));
+			//
+			// var endLiveEdit = AccessTools.Method(AnimationWindowDopeSheetAccess.GetAnimEditorType(), "OnEndLiveEdit");
+			// if (endLiveEdit != null)
+			// 	instance.Patch(endLiveEdit, new HarmonyMethod(AccessTools.Method(typeof(AnimationWindowDopeSheetGUI_Patch), nameof(OnStartLiveEdit_Prefix))));
+			
+			var animWindowCurve = AccessTools.Method(AnimationWindowDopeSheetAccess.GetAnimationWindowCurveType(), "ComparePaths");
+			if (animWindowCurve != null)
+				instance.Patch(animWindowCurve, new HarmonyMethod(AccessTools.Method(typeof(AnimationWindowDopeSheetGUI_Patch), nameof(OnComparePaths))));
+		}
+
+		// private static bool DopeLinesGUI_Prefix(object __instance, Rect position, Vector2 scrollPosition)
+		// {
+		// 	// Debug.Log(__instance);
+		// 	// Debug.Log(position);
+		// 	// Debug.Log(scrollPosition);
+		// 	// return false;
+		// 	return true;
+		// }
+		//
+		// private static bool OnStartLiveEdit_Prefix()
+		// {
+		// 	return true;
+		// }
+
+		private static bool OnComparePaths(object __instance, string otherPath, ref int __result)
+		{
+			if (AnimationWindowDopeSheetAccess.CompareAnimWindowCurvePaths(__instance, otherPath, out var res))
+			{
+				__result = res;
+				return false;
+			}
+			
+			return true;
+		}
+	}
+}
---
+++ package/Editor/MissingClipBindings/Internals/AnimationWindowDopeSheetAccess.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace Needle.AnimationUtils
+{
+	public class AnimationWindowDopeSheetAccess
+	{
+		[InitializeOnLoadMethod]
+		private static void Init()
+		{
+			// DopeSheetEditor.
+			// AnimEditor
+			// AnimationWindowState
+		}
+		
+		public static Type GetDopeSheetType() => typeof(DopeSheetEditor);
+		public static Type GetAnimEditorType() => typeof(AnimEditor);
+		public static Type GetAnimationWindowCurveType() => typeof(AnimationWindowCurve);
+
+		
+		
+		
+		private static readonly Dictionary<string, string[]> splitPaths = new Dictionary<string, string[]>();
+
+		public static bool CompareAnimWindowCurvePaths(object animWindowCurve, string otherPath, out int res)
+		{
+			if (animWindowCurve is AnimationWindowCurve curve)
+			{
+				if (!splitPaths.TryGetValue(curve.path, out var thisPath)) 
+					thisPath = splitPaths[curve.path] = curve.path.Split('/');
+				if (!splitPaths.TryGetValue(otherPath, out var objPath)) 
+					objPath = splitPaths[otherPath] = otherPath.Split('/');
+				res = OriginalCompare(thisPath, objPath);
+				return true;
+			}
+
+			res = 0;
+			return false;
+
+		}
+
+		private static int OriginalCompare(string[] thisPath, string[] objPath)
+		{
+			int smallerLength = Math.Min(thisPath.Length, objPath.Length);
+			for (int i = 0; i < smallerLength; ++i)
+			{
+				int compare = string.Compare(thisPath[i], objPath[i], StringComparison.Ordinal);
+				if (compare == 0)
+				{
+					continue;
+				}
+
+				return compare;
+			}
+
+			if (thisPath.Length < objPath.Length)
+			{
+				return -1;
+			}
+
+			return 1;
+		}
+	}
+}
